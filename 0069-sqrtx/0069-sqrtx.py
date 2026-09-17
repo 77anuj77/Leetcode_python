84c44a6ec5897f3c6589b937a69b6a1a487cb5eb
Anuj Paroha <anujparoha07@gmail.com>
2026-09-17 11:01:14 +0530

    Time: 1 ms (73.49%), Space: 12.5 MB (19.77%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.py
+++ 0069-sqrtx/0069-sqrtx.py
@@ -1,19 +1,60 @@
 class Solution(object):
     def mySqrt(self, x):
-        if x < 2:
+
+        if x == 0 or x == 1:
             return x
 
-        left = 1
-        right = x
+        low = 1
+        high = x
 
-        while left <= right:
-            mid = (left + right) // 2
+        while low <= high:
+            mid = (low + high) // 2
 
             if mid * mid == x:
                 return mid
-            elif mid * mid < x:
-                left = mid + 1
-            else:
-                right = mid - 1
 
-        return right
+            elif mid * mid < x:
+                low = mid + 1
+
+            else:
+                high = mid - 1
+
+        return high
+
+        '''
+Time complexity: O(log x)
+Space complexity: O(1)
+
+START
+
+Take the number x
+
+If x is 0 or 1
+    return x
+
+Set left = 1
+Set right = x
+
+While left is smaller than or equal to right
+
+    Find the middle number
+        mid = middle of left and right
+
+    If mid × mid equals x
+        return mid
+
+    If mid × mid is smaller than x
+        Move to the right side
+        left = mid + 1
+
+    Otherwise
+        Move to the left side
+        right = mid - 1
+
+When the search ends
+    return right
+
+END
+
+        '''
+       
