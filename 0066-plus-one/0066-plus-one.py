ded0def68b62cc3ee01fa67c514a420af51e4d51
Anuj Paroha <anujparoha07@gmail.com>
2026-09-17 09:53:31 +0530

    Time: 0 ms (100%), Space: 12.3 MB (90.16%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.py
@@ -0,0 +1,29 @@
+class Solution(object):
+    def plusOne(self, digits):
+        """
+        :type digits: List[int]
+        :rtype: List[int]
+        """
+        if not digits:
+            return 0
+
+        for i in range(len(digits)-1, -1,-1):
+            if digits[i]<9:
+                digits[i]+=1
+                return digits
+
+            else :
+                digits[i]=0
+            
+
+        digits.insert(0,1)
+        return digits
+
+                
+
+            
+            
+
+            
+
+            
