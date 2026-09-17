e5b14f35ac60a4900233c90dfa5360a238b0147a
Anuj Paroha <anujparoha07@gmail.com>
2026-09-17 15:00:54 +0530

    Time: 6 ms (98.84%), Space: 12.9 MB (55.58%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,24 +1,37 @@
+# class Solution(object):
+#     def isPalindrome(self, s):
+        # left = 0
+        # right = len(s) - 1
+
+        # while left < right:
+
+        #     # Skip non-alphanumeric characters
+        #     if not s[left].isalnum():
+        #         left += 1
+        #         continue
+
+        #     if not s[right].isalnum():
+        #         right -= 1
+        #         continue
+
+        #     # Compare characters
+        #     if s[left].lower() != s[right].lower():
+        #         return False
+
+        #     left += 1
+        #     right -= 1
+
+        # return True
+
 class Solution(object):
     def isPalindrome(self, s):
-        left = 0
-        right = len(s) - 1
-
-        while left < right:
-
-            # Skip non-alphanumeric characters
-            if not s[left].isalnum():
-                left += 1
-                continue
-
-            if not s[right].isalnum():
-                right -= 1
-                continue
-
-            # Compare characters
-            if s[left].lower() != s[right].lower():
-                return False
-
-            left += 1
-            right -= 1
-
-        return True
+        s=s.lower()
+        sc=' ,@:.&*#$%"!?_-+=/|{[]}():;><^`\~'
+        for i in sc:
+            s=s.replace(i,'')
+        s=s.replace("'",'')
+        left=0
+        right=len(s)-1
+        if s==s[::-1]:
+            return True
+        return False
