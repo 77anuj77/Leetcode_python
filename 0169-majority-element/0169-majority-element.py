20ebfd33aa38b5c1926305cceedae7ec9ad0001c
Anuj Paroha <anujparoha07@gmail.com>
2026-09-19 18:35:35 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -4,12 +4,11 @@ class Solution(object):
         count = 0
 
         for num in nums:
-            if count == 0:
-                candidate = num
+            if count==0:
+                candidate==num
+            if num==candidate:
+                count+=1
 
-            if num == candidate:
-                count += 1
             else:
-                count -= 1
-
+                count-=1
         return candidate
