115ebbe8cf239c5a637394d1b5b488bd12b34cf2
Anuj Paroha <anujparoha07@gmail.com>
2026-09-19 19:29:38 +0530

    Time: 98 ms (5.18%), Space: 32.7 MB (7.65%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        d={}
+        for i in nums:
+            if i in d:
+                d[i]+=1
+            else:
+                d[i]=1
+        
+        flag=False
+        for k, v in d.items():
+            if v>1:
+                flag= True
+        return flag
