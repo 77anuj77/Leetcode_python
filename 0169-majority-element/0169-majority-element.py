2b1b12598444c40bd4b85ee0157db7b17d73fc65
Anuj Paroha <anujparoha07@gmail.com>
2026-09-19 18:42:20 +0530

    Time: 19 ms (15.32%), Space: 13.6 MB (74.7%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,14 +1,17 @@
 class Solution(object):
     def majorityElement(self, nums):
-        candidate = None
-        count = 0
+        
+        n=len(nums)
+        d={}
 
-        for num in nums:
-            if count==0:
-                candidate=num
-            if num==candidate:
-                count+=1
+        for i in nums:
+            if i in d:
+                d[i]+=1
 
             else:
-                count-=1
-        return candidate
+                d[i]=1
+        maj=0
+        for k,v in d.items():
+            if v>(n/2):
+                maj=k
+        return maj
